3df359e50887f27e9247b67dd159f4a079f7914b
nikita <git@github.com:Nikitak211/temp-node.js.git>
2021-08-16 22:29:07 +0300

    server setup

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('./public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve( __dirname, './snippet/index.html'))
+    res.sendFile(path.resolve( __dirname, './public/index.html'))
 })
 app.all('*', (req, res) => {
     res.status(404).send('<h1>Home page not found</h1>')
