695c00a1fe26c0c948fa1adce16370e776c60653
nikita <git@github.com:Nikitak211/temp-node.js.git>
2021-08-17 18:04:14 +0300

    styles and script update

--- public/script.js
+++ public/script.js
@@ -1,2 +1,27 @@
-const fs = require('fs')
+const post = document.getElementById('but');
+const text = document.getElementById('label')
+
+function e() {
+	toggleClass(document.getElementById('submit'), 'click');
+	}
+	form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+        submit();
+	});
+
+    function submit(){
+
+        const parent  = document.getElementById('parent')
+        const child = document.createElement('li')
+
+        const total = document.createTextNode(generator);
+        child.appendChild(total);
+        parent.replaceChild(child, parent.childNodes[0]);
+
+
+    }
+    
+
+
 
