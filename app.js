04a8d0cb84309ce63ed50c951ff825351e742634
nikita <git@github.com:Nikitak211/temp-node.js.git>
2021-08-18 17:23:31 +0300

    express modification

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const path = require('path');
+const logger = require('./midware/logger')
+const members = require('./members')
 const app = express();
 
-app.use(express.static('./public'))
+// init middleware
+// app.use(logger);
+
+
+
+
+
+app.get('/api/members', (req, res) => res.json(members));
+
+//Get Single Member
+app.get('/api/members/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id))
+    
+    if(found){
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    } else{
+        res.status(400).json({msg: `No member with the id of ${req.params.id}` });
+    }
+})
+
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve( __dirname, './public/index.html'))
 })
+
 app.all('*', (req, res) => {
     res.status(404).send('<h1>Home page not found</h1>')
 })
 
-app.listen(5000, ()=> {
-    console.log('listening on port 5000....')
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=> console.log('listening on port 5000....'));
---
+++ members.js
@@ -0,0 +1,19 @@
+const members = [
+    {
+        id: 1 ,
+        name: 'nikita',
+        username: 'Nikita'
+    },
+    {
+        id: 2 ,
+        name: 'leo',
+        username: '808'
+    },
+    {
+        id: 3 ,
+        name: 'david',
+        username: 'Gitmanhub'
+    }
+];
+
+module.exports = members;
