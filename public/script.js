a96a10d127d20451260f82f981a792143e0204d1
8o8inCodes <8o8inCodes>
2021-08-19 19:03:33 +0300

    Add instructions

--- public/script.js
+++ public/script.js
@@ -1,3 +1,9 @@
+// Find the button
+// when you click the button, it should call a function "getSnippet"
 
 
-
+const getSnippet = () => {
+    // call the server using "fetch" to '/generateText' API route
+    // wait for it to return response (read about promises and async await), take the text that it provided
+    // find the "text-snippet" element and set it's text to the response's text 
+}
