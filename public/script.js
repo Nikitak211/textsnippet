e3997915a250590323d2bf2f7dc26d11daa9b0f8
nikita <git@github.com:Nikitak211/temp-node.js.git>
2021-08-22 19:00:38 +0300

    Final

--- public/script.js
+++ public/script.js
@@ -1,12 +1,22 @@
 // Find the button
 // when you click the button, it should call a function "getSnippet"
 const button = document.getElementById('but');
-submit.addEventListener('click', getSnippet);
+button.addEventListener('click', getSnippet);
 
-const getSnippet = () => {
+const text = fetch('http://localhost:5000/generateText')
+.then(response => response.text().then(data => paragraph.innerHTML = data ))
+;
+
+const paragraph = document.getElementById('text-snippet');
+
+function getSnippet() {
     // call the server using "fetch" to '/generateText' API route
     // wait for it to return response (read about **promises** and **async await**), take the text that it provided
-    // find the "text-snippet" element and set it's text to the response's text 
-    const paragraph = document.getElementById('text-snippet');
+    // find the "text-snippet" element and set it's text to the response's text
+    return new Promise((resolve, reject) => {
+        if (getSnippet()){
+            resolve()
+        }
+    })
 
 }
