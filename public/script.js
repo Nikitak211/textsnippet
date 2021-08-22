138d900c76d29c0c1863a044eb11b63723f02afe
nikita <git@github.com:Nikitak211/temp-node.js.git>
2021-08-22 20:14:14 +0300

    fix Comunication With the server

--- public/script.js
+++ public/script.js
@@ -3,9 +3,7 @@
 const button = document.getElementById('but');
 button.addEventListener('click', getSnippet);
 
-const text = fetch('http://localhost:5000/generateText')
-.then(response => response.text().then(data => paragraph.innerHTML = data ))
-;
+
 
 const paragraph = document.getElementById('text-snippet');
 
@@ -13,9 +11,8 @@ function getSnippet() {
     // call the server using "fetch" to '/generateText' API route
     // wait for it to return response (read about **promises** and **async await**), take the text that it provided
     // find the "text-snippet" element and set it's text to the response's text
-    return new Promise((resolve, reject) => {
-        if (getSnippet()){
-            resolve(text)
-        }
-    })
+    fetch('/generateText')
+        .then(response => response.text()
+        .then(data => paragraph.innerHTML = data ))
+;
 }
