dbbca8e461c666f87a524f2406cfd36087ee0eff
nikita <git@github.com:Nikitak211/temp-node.js.git>
2021-09-14 20:13:12 +0300

    delteted for loop and fixed copied text.

--- midware/midware.js
+++ midware/midware.js
@@ -3,12 +3,8 @@ const txtgen = require('txtgen');
 
 const textgen = (req, res) => {
     const sum = req.body.score;
-    const textsnippet = txtgen.sentence();
-    const array = []
-    for(let i = 0; i < sum ; i++){
-        array.push([textsnippet])
-    }
-    res.send(array.toString());
+    const textsnippet = txtgen.paragraph([sum])
+        res.send(textsnippet);
 }
 
 module.exports = {
