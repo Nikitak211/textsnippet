3ef665b87d8aa350ae791bf212bfb9a10fc7c41d
nikita <git@github.com:Nikitak211/temp-node.js.git>
2021-09-22 16:16:52 +0300

    bugs in switch

--- api/textsnippetRoutes.js
+++ api/textsnippetRoutes.js
@@ -1,22 +1,13 @@
-/** @module router */
+
 const express = require("express");
 const txtgen = require('txtgen');
 const router = express.Router();
 
-/** @typedef {object} post posting to a route from an api. */
 router.post('/generateText' , (req, res) => {
 
-    /**
-    * @typedef {Number} Sum requesting the body data of score {@link requestPayload}. 
-    */
     //gets the body of score from the fetch option.
     const sum = req.body.score;
 
-    /**
-    * @typedef {String} txtgen an npmjs util that generates random sentences, paragraphs and articles in English.
-    * links to {@link textsnippet}. 
-    * you can check more at {@link https://www.npmjs.com/package/txtgen}
-    */
     //generates a random text .
     const textsnippet = txtgen.paragraph([sum])
 
@@ -31,9 +22,6 @@ router.post('/generateText' , (req, res) => {
         res.send({error: 'Number Of Paragraphs cannot be lower than 1.'})
     } else {
         //sends data to the adress /generateText .
-        /**
-        * @typedef {string} textsnippet sends data to the route /generateText as {@link data} to textSnippet .  
-        */
         res.send(textsnippet);
     }
 })
--- public/script.js
+++ public/script.js
@@ -1,46 +1,17 @@
 // all elements for the script from the index .
 
-/**
- * @typedef {elements} input equal to elements of id. 
- */
 const inputs = document.getElementById('paragraphInput');
-
-/**
- * @typedef {elements} paragraph equal to elements of id. 
- */
 const paragraph = document.getElementById('text-snippet');
-
-/**
- * @typedef {elements} button equal to elements of id. 
- */
 const button = document.getElementById('generate-snippet-btn');
 
-/**
- * @typedef {EventListener} button create an event listener for button, and sends getSnippet function. 
- */
 // event listener for click events.
 button.addEventListener('click', getSnippet);
 
-
-/**
- * @param {function} getSnippet on the event listener sends a request to the api and fetches the snippet.
- */
 function getSnippet() {
 
-    /**
-    * @typedef {numbers} amountOfSnippetsToGenerate sends the value of inputs. 
-    */
-    // getting the value of the input.
-    const amountOfSnippetsToGenerate = inputs.value;
-
-    /**
-    * @typedef {{score: number}} requestPayload sends json to the api.
-    */   
+    const amountOfSnippetsToGenerate = inputs.value; 
     const requestPayload = {score: amountOfSnippetsToGenerate};
     
-    /**
-    * @typedef {json} options options for the fetch method. 
-    */
     // fetch options.
     const options = {
       method: "POST",
@@ -48,9 +19,6 @@ function getSnippet() {
       headers: { "Content-type": "application/json; charset=UTF-8" }
     };
 
-    /**
-    * @typedef {Statements} Statements Conditional Statements to search for errors. 
-    */
     // checks for letters and empty spaces, and bug preventing ..
       if ( amountOfSnippetsToGenerate === "") {
         setErrorFor( inputs, 'Number Of Paragraphs , field cannot be empty.')
@@ -61,10 +29,6 @@ function getSnippet() {
     } else if ( amountOfSnippetsToGenerate <= 0 ) {
         setErrorFor( inputs, 'Number Of Paragraphs cannot be lower than 1.')
     } else {
-        
-        /**
-        * @typedef {fetch} promise fetching data from the api {@link }. 
-        */
         fetch( '/api/generateText', options )
         .then( response => response.text()
         .then( ( data ) => {
@@ -73,21 +37,14 @@ function getSnippet() {
             } else if (data === "") {
                 alert('Unknown  error has occurred.')
             } else {
-                /** 
-                * @typedef {string} data sending an textsnippet from the api 
-                * and lets {@link speech} read it. 
-                */
-                // sending data and speaking .
-                toggle(data)
                 paragraph.innerHTML = data
-                setSucces(inputs)
+                setSucces(inputs) 
             }
         })
     )}
+   
 }
-                /** 
-                * @typedef {function} setErrorFor sends error if an error ocured
-                */
+
 // Trigers Error 
 function setErrorFor(input, message) {
 	const formControl = input.parentElement;
@@ -95,14 +52,14 @@ function setErrorFor(input, message) {
 	formControl.className = 'inputs-form error';
 	small.innerText = message;
 };
-                /** 
-                * @typedef {function} setSucces removes all warning messages if no error was found.
-                */
+
 // Trigers success
 function setSucces(input, message) {
 	const formControl = input.parentElement;
 	formControl.className = 'inputs-form';
 };
+
+
 const mute = document.getElementById("on");
 mute.addEventListener('click',toggle);
 const p = document.getElementById("output")
@@ -128,3 +85,14 @@ function toggle(read){
         
     }
 }
+
+// const repeat = document.getElementById("repeat");
+// repeat.addEventListener('click',repeat)
+
+// function repeat(data){
+
+//     let speech = new SpeechSynthesisUtterance();
+//     speech.lang = "en";
+//     speech.text = data;
+//     window.speechSynthesis.speak(speech)
+// }
